40118eb167b5ea7cbc6a287d3c0926da672ae270
Melvin Lindholm <meli0703@storsjogymnasiet.se>
2023-04-06 10:51:56 +0200

    ADD js

---
+++ gissa/gissa.js
@@ -0,0 +1,10 @@
+const slumptal = Math.floor(Math.random() * 9) + 1
+let antalgissningar = 0
+console.log("slumptalet:" + slumptal)
+function gissaTal(){
+    let textfelt_gissa = document.getElementById("gissa").value
+    let gissa_int = Number(textfelt_gissa)
+    let vinn = false
+    let svar = document.getElementById("svar")
+    console.log("textfelt=" + textfelt_gissa)
+}
