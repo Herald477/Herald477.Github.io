a1f87d4dd05a72902536d12d426be8bd5af2a0f7
Melvin Lindholm <meli0703@storsjogymnasiet.se>
2023-05-11 10:25:24 +0200

    ADD html css js + BUTTON works

---
+++ Växla/Växla.js
@@ -0,0 +1,10 @@
+const input_pris = document.getElementById("pris")
+const input_inbet = document.getElementById("inbet")
+const svardiv = document.getElementById("svar")
+const kalkulator = document.getElementById("kalk")
+
+kalkulator.addEventListener("click", kalkulatorClick)
+
+function kalkulatorClick(){
+    console.log(`click`)
+}
